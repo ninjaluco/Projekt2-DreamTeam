a053de67f77ae09ec41dd2ed7cef56d9b572228b
Nicolas Luco <nico_luco@hotmail.com>
2017-10-17 15:13:05 +0200

    Artiklar.CS ,ArtikelRegistrering, ReadAllArtiklar,  UpdateArtikel

---
+++ Index/SqlLibrary/Artiklar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlLibrary
+{
+    public class Artiklar
+    {
+        public int AID { get; set; }
+        public string artikelnamn { get; set; }
+        public int pris { get; set; }
+        public string kategori { get; set; }
+     
+    }
+}
